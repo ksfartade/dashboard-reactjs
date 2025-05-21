eda81df014cd73e675c9052729937897c24d9882
ksfartade <fartadek@gmail.com>
2025-05-21 21:25:07 +0530

    designed employees page.

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Sort,
+  Inject,
+  Edit,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
 
 const Employees = () => {
   return (
-    <div>Employees</div>
-  )
-}
+    <div className="w-full box-border  min-h-[calc(100vh-100px)] p-[4vw] flex">
+      <div className="flex-1 p-[4vw] bg-white">
+        <Header category="Page" title="Employees" />
 
-export default Employees
+        <div className="mt-10 w-full">
+          <GridComponent
+            dataSource={employeesData}
+            allowPaging={true}
+            allowSorting={true}
+            pageSettings={{ pageSize: 10 }}
+            editSettings={{
+              allowEditing: true,
+              allowAdding: true,
+              allowDeleting: true,
+              mode: 'Normal', // or 'Normal' | 'Batch'
+            }}
+            toolbar={['Search']}
+          >
+            <ColumnsDirective>
+              {
+                employeesGrid.map((item, index) => (
+                  <ColumnDirective
+                    key={index} {...item}
+                  />
+                ))
+              }
+            </ColumnsDirective>
+
+            <Inject services={[Page, Sort, Search, Edit, Toolbar]} />
+          </GridComponent>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Employees;
